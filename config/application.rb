290ece6912423687cfc5505bc96acbd209635c57
Kishor Budhathoki <budhathokikishor10@gmail.com>
2020-04-02 16:42:43 +0200

    Updated secrets base call and prodiction environment

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module StudentProfile
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.eager_load_paths << "#{Rails.root}/lib"
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
